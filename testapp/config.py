008a55303b3316b2a552c54f47bdb794043a0396
rainyt4iyo <A_t4iyo@outlook.com>
2025-08-01 01:12:06 +0900

    new

--- testapp/config.py
+++ testapp/config.py
@@ -1,1 +1,17 @@
-DEBUG = True
+import os
+
+class Config:
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
