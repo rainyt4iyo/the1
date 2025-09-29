54d2d42e3130bf07cf6348eaa254b7a90a16fb3e
rainyt4iyo <A_t4iyo@outlook.com>
2025-09-29 16:01:48 +0900

    9/29 ogikubo

--- testapp/views.py
+++ testapp/views.py
@@ -7,3 +7,11 @@ from contextlib import contextmanager
 @app.route('/')
 def mainpage():
     return render_template('testapp/mainpage.html')
+
+@app.route('/rules')
+def rules():
+    return render_template('testapp/rules.html')
+
+@app.route('/ranking/<category>')
+def ranking(category):
+    return render_template('testapp/ranking.html', category=category)
