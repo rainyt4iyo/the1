90341689384e3462ee37aec3aaaebed65e82e23f
rainyt4iyo <A_t4iyo@outlook.com>
2025-11-01 16:03:43 +0900

    sc

--- testapp/views.py
+++ testapp/views.py
@@ -378,7 +378,8 @@ def ranking(category):
                        database='the1',
                        cursorclass=pymysql.cursors.DictCursor)
     cursor = conn.cursor()
-
+    category = categorytranslateWithBrank(category)
+    category = category.replace("\n", "<br>")
     try:
         with conn.cursor() as cursor:
             sql = f"SELECT * from {category} ORDER BY id"
@@ -388,8 +389,7 @@ def ranking(category):
                 category_binary = True
             elif category == "f_asp_wmn":
                 category_binary = True
-            category = categorytranslateWithBrank(category)
-            category = category.replace("\n", "<br>")
+
             data = cursor.fetchall()
     finally:
         conn.close()
@@ -400,7 +400,10 @@ def ranking(category):
         scorecalc(i)
         omitName(i)
     data = sorted(data, key=lambda x: (-x['total'], x['id'] if x['id'] is not None else float('inf')))
-    del data[-63:]
+    if category == 'asp_men':
+        del data[-63:]
+    elif category == 'asp_wmn':
+        del data[-88:]
     if category_binary == True:
         return render_template('testapp/ranking_asp.html', category=category, data=data)
     else:
