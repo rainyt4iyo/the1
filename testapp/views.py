e4fa08aee63341ac730123564e3e22e220264414
rainyt4iyo <A_t4iyo@outlook.com>
2025-11-01 15:51:10 +0900

    pop off

--- testapp/views.py
+++ testapp/views.py
@@ -400,6 +400,7 @@ def ranking(category):
         scorecalc(i)
         omitName(i)
     data = sorted(data, key=lambda x: (-x['total'], x['id'] if x['id'] is not None else float('inf')))
+    data = data.pop(-62)
 
     if category_binary == True:
         return render_template('testapp/ranking_asp.html', category=category, data=data)
